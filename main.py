eb5f3a911a8b7ab2efb383c92be3dd9ca5c080e4
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-20 22:09:30 +0000

    functions with outputs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#functions with outputs
+#functions with outputs 
 def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "Invalid input"
   return f_name.capitalize() + " " + l_name.capitalize()
-print(format_name("megAn", "lUKES"))
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
